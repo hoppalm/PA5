2ae303a7ed7b71dc728fa7f210819de52708744e
Michael_Hoppal <hoppalm@gmail.com>
2014-11-30 22:26:26 -0700

    f

--- CUDA1/vecAddKernel01.cu
+++ CUDA1/vecAddKernel01.cu
@@ -2,20 +2,13 @@
 
 __global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
-    /*int blockStartIndex  = blockIdx.x * blockDim.x * N;
+	int blockStartIndex  = blockIdx.x * blockDim.x * N;
     int threadStartIndex = blockStartIndex + (threadIdx.x * N);
     int threadEndIndex   = threadStartIndex + N;
     int i;
-
-    for( i=0; i<N; i++ ){
-		C[threadStartIndex + (blockDim.x * i)] = A[threadStartIndex + (blockDim.x * i)] + B[threadStartIndex + (blockDim.x * i)];
-    }*/
-
-	int blockStartIndex  = blockIdx.x * blockDim.x * N;
-	int threadStartIndex = blockStartIndex + threadIdx.x; 
-	int pos, i;
-	for (i=0; i<N; i++){
-		pos = threadStartIndex + ( i * blockDim.x);
-		C[pos] = A[pos] + B[pos];
-	}
+    int add;
+    for( i=threadStartIndex; i<threadEndIndex; i++ ){
+    	add = ((i - threadStartIndex)*blockDim.x) + threadStartIndex;
+		C[add] = A[add] + B[add];
+    }
 }
