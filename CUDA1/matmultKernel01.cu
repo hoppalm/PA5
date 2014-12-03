c23a50c001a4f8f17c466e50789c91cb869fce4f
Michael_Hoppal <hoppalm@gmail.com>
2014-12-03 15:24:48 -0700

    yeahhyeahh

--- CUDA1/matmultKernel01.cu
+++ CUDA1/matmultKernel01.cu
@@ -73,7 +73,7 @@ __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
        Cvalue += shared_A[thread_row][e] * shared_B[e][thread_col];
        Cvalue1 += shared_A[thread_row+16][e] * shared_B[e][thread_col];
        Cvalue2 += shared_A[thread_row][e] * shared_B[e][thread_col+16];
-       Cvalue3 += shared_A[thread_row+16][e] * shared_B[e][thread_col+16];
+       Cvalue3 += shared_A[thread_row+100000][e] * shared_B[e][thread_col+16];
    }
 
 
